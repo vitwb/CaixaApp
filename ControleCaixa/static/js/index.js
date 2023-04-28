b982f18510df574fd744dea3fa5b2e104b2709e4
Vitor Barbosa <vitor.v.b13@gmail.com>
2023-04-28 14:55:16 -0300

    teste qrcode

--- ControleCaixa/static/js/index.js
+++ ControleCaixa/static/js/index.js
@@ -1,18 +1,31 @@
+
+
+
 function onScanSuccess(decodedText, decodedResult) {
-    // handle the scanned code as you like, for example:
-    console.log(`Code matched = ${decodedText}`, decodedResult);
-  }
+  // handle the scanned code as you like, for example:
   
-  function onScanFailure(error) {
+  var elem = document.getElementById("reader");
+  elem.remove();
+  console.log("OSS");
+  document.getElementById('resultado').innerHTML=`<a>"${decodedText}"</a>`
+  html5QrcodeScanner.clear();
+}
+
+function onScanFailure(error) {
     // handle scan failure, usually better to ignore and keep scanning.
     // for example:
     console.warn(`Code scan error = ${error}`);
   }
   
-  let html5QrcodeScanner = new Html5QrcodeScanner(
+
+    
+document.getElementById("CriarButton").onclick = function(){
+  const html5QrcodeScanner = new Html5QrcodeScanner(
     "reader",
-    { fps: 10, qrbox: {width: 250, height: 250} },
+    { fps: 20, qrbox: {width: 250, height: 250} },
     /* verbose= */ true);
-  html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+    html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+};
+
 
  
