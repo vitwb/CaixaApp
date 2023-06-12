e3f288b02da67abc39ab81d4b3701c4fff8cf772
Vitor Barbosa <62035588+vitwb@users.noreply.github.com>
2023-06-12 17:14:08 -0300

    Update settings.py

--- ControleCaixa/ControleCaixa/settings.py
+++ ControleCaixa/ControleCaixa/settings.py
@@ -123,9 +123,7 @@ STATICFILES_DIRS = (
 STATIC_URL = '/static/'
 
 
-STATIC_ROOT = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATIC_ROOT = '/static/'
 
 LOGIN_REDIRECT_URL='/'
 LOGOUT_REDIRECT_URL = '/login'
